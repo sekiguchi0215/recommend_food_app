8ba699fb7417c7074c1a77c3c41f1cf90997c4a4
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-17 20:29:14 +0900

    リレーション 及び バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+  has_many :foods, dependent: :destroy
+
+  validates :name, presence: true
 end
