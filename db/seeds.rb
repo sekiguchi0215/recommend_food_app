4e8c53ff768ec718b15cd33ee3985c62a5e61eee
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-17 22:11:27 +0900

    初期データ作成中

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+email = "test@example.com"
+password = "password"
 
-User.create!(email: "test@example.com", password: "password")
-# puts "ユーザーの初期データインポートに成功しました。"
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE foods RESTART IDENTITY CASCADE")
+
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "tanaka@example.com", password: "password")
+user3 = User.create!(email: "suzuki@example.com", password: "password")
+
+user1.foods.create!(name: "玉子焼き")
+user1.foods.create!(name: "味噌汁")
+user2.foods.create!(name: "焼き魚")
+user3.foods.create!(name: "たくあん")
+user3.foods.create!(name: "コーンフレーク")
+
+User.create!(email: email, password: password)
+
+puts "ユーザーの初期データインポートに成功しました。"
