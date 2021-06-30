436699ff62a4984259e25a73e19afdcff4983650
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-30 16:24:27 +0900

    詳細ページにいいね機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "foods#index"
-  resources :food do
+  resources :foods do
     resource :likes, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
