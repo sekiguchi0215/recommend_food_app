f048a348671425ba1dc62719d5f4a63d3b96855d
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-30 16:12:06 +0900

    Like モデルのコントローラを作成 及び ルーティングを記述

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :food
   validates :user_id, uniqueness: {
-    scope: food_id,
+    scope: :food_id,
     message: "同じ投稿に2回いいねはできません。"
   }
 end
